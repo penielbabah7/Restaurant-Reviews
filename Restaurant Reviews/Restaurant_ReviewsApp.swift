2c2eec73c034211172f8876e1f55fa00ef3d7d23
Peniel Babah <penielbabah@cedarville.edu>
2024-08-28 21:38:09 -0400

    Initial Commit

---
+++ Restaurant Reviews/Restaurant_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ReviewsApp.swift
+//  Restaurant Reviews
+//
+//  Created by Peniel Babah on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
